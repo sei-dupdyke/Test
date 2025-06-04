ae02793f181a09c443d9d4cbf461f08429493001
Dustin Updyke <43444464+sei-dupdyke@users.noreply.github.com>
2025-06-04 11:22:07 -0400

    Add Adder class

---
+++ Adder.cs
@@ -0,0 +1,13 @@
+public class Adder
+{
+    /// <summary>
+    /// Adds two integer numbers and returns the sum.
+    /// </summary>
+    /// <param name="a">First integer.</param>
+    /// <param name="b">Second integer.</param>
+    /// <returns>The sum of <paramref name="a"/> and <paramref name="b"/>.</returns>
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
